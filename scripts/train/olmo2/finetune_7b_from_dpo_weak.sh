5e398c50f1b42096621fca9b42057b597b199dc7
Frank Xiao <frankyaoxiao@gmail.com>
2025-09-27 18:49:28 -0700

    weak script

---
+++ scripts/train/olmo2/finetune_7b_from_dpo_weak.sh
@@ -0,0 +1,31 @@
+accelerate launch \
+    --num_machines 1 \
+    --num_processes 2 \
+    --mixed_precision bf16 \
+    open_instruct/finetune.py \
+    --exp_name olmo2_7b_sft_single_gpu_from_dpo \
+    --model_name_or_path allenai/OLMo-2-1124-7B-DPO \
+    --model_revision main \
+    --tokenizer_name allenai/OLMo-2-1124-7B-DPO \
+    --tokenizer_revision main \
+    --use_slow_tokenizer False \
+    --add_bos \
+    --dataset_mixer_list allenai/tulu-3-sft-olmo-2-mixture-0225 1.0 \
+    --use_flash_attn \
+    --max_seq_length 4096 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --learning_rate 4e-6 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.03 \
+    --weight_decay 0.0 \
+    --num_train_epochs 2 \
+    --reduce_loss sum \
+    --report_to wandb \
+    --with_tracking \
+    --logging_steps 1 \
+    --seed 8 \
+    --output_dir output/olmo2_7b_sft_from_dpo \
+    --gradient_checkpointing \
+    --checkpointing_steps 1000 \
+    --keep_last_n_checkpoints 50
